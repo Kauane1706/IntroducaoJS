3add9e2914347945a03b89d015b1cbb17b476bb7
Kauane1706 <kauanekakadossantos854gmail.com>
2025-06-25 09:37:56 -0300

    adiciona funcionalidades de subtração, multiplicação e divisão em ex005

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -4,3 +4,28 @@ function somar(){
     let soma = parseFloat(num1) + parseFloat(num2);
     document.getElementById("resultado").innerHTML = "Resultado: " + soma;
 }
+
+function subtrair(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let subtracao = parseFloat(num1) - parseFloat(num2);
+    document.getElementById("resultado").innerHTML = "Resultado: " + subtracao;
+}
+
+function multiplicar(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let multiplicacao = parseFloat(num1) * parseFloat(num2);
+    document.getElementById("resultado").innerHTML = "Resultado: " + multiplicacao;
+}
+
+function dividir(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    if (parseFloat(num2) === 0) {
+        document.getElementById("resultado").innerHTML = "Erro: Divisão por zero!";
+    } else {
+        let divisao = parseFloat(num1) / parseFloat(num2);
+        document.getElementById("resultado").innerHTML = "Resultado: " + divisao;
+    }
+}
